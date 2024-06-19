b7c8b9f4ef617a6487d53200ee61bc869676c366
Alexander Dobromilskiy <alexdob.twist@gmail.com>
2024-06-19 20:48:22 +0300

    create tests for pinger

---
+++ pinger/pinger_test.go
@@ -0,0 +1,42 @@
+package pinger
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/adobromilskiy/pingatus/config"
+	"github.com/adobromilskiy/pingatus/mock"
+)
+
+func TestPingerDo(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	cfg := &config.Config{
+		HTTPPoint: []config.HTTPpointConfig{{
+			Name:     "test",
+			URL:      server.URL,
+			Status:   http.StatusOK,
+			Interval: 300 * time.Millisecond,
+			Timeout:  100 * time.Millisecond,
+		}},
+	}
+
+	store := &mock.StoreMock{}
+	notifier := &mock.NotifierMock{}
+
+	pinger := NewPinger(cfg, store, notifier)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		cancel()
+	}()
+	pinger.Do(ctx)
+}
