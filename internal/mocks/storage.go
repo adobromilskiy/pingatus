f26369b23370e3a04be3fef579a38900b18a58f2
Alexander Dobromilskiy <alexdob.twist@gmail.com>
2024-12-24 15:25:00 +0200

    mocks: add new methods

--- internal/mocks/storage.go
+++ internal/mocks/storage.go
@@ -1,17 +1,43 @@
 package mocks
 
 import (
+	"context"
 	"errors"
+	"fmt"
 
 	"github.com/adobromilskiy/pingatus/core"
 )
 
 type StorageMock struct{}
 
-func (s *StorageMock) Save(data core.Endpoint) error {
+func (s *StorageMock) Save(_ context.Context, data core.Endpoint) error {
 	if data.Name != "test" {
 		return errors.New("test: wrong name") //nolint:goerr113
 	}
 
 	return nil
+}
+
+func (s *StorageMock) GetEndpoints(_ context.Context) ([]string, error) {
+	return []string{"test1", "test2"}, nil
+}
+
+func (s *StorageMock) GetEndpointStats(_ context.Context, name string, date int64) ([]core.Endpoint, error) {
+	if name != "test" {
+		return nil, fmt.Errorf("test: wrong name")
+	}
+
+	return []core.Endpoint{
+		{
+			Name:    name,
+			Address: "http://localhost",
+			Status:  true,
+			Date:    date,
+		}, {
+			Name:    name,
+			Address: "http://localhost",
+			Status:  true,
+			Date:    date,
+		},
+	}, nil
 }
